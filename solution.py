56ecc74297c48fcefa35d57067641f7ff6f055e5
Tomás Mónaco <monacotomas99@gmail.com>
2023-07-07 14:16:55 +0200

    Added basic bouncing

--- solution.py
+++ solution.py
@@ -1,8 +1,17 @@
 import pygame
+pygame.init()
 
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('PONG')
+
+FPS = 60
+
+WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+BALL_RADIUS = 7
 
 class Paddle:
     COLOR = WHITE
@@ -22,6 +31,63 @@ class Paddle:
             self.y -= self.VEL
         else:
             self.y += self.VEL
+
+class Ball:
+    # COLOR = WHITE
+    COLOR = (230, 230, 5)
+    MAX_VEL = 4
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win): 
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+
+def draw(win, paddles, ball):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    # Draw a dashed line in the middle of the screen
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+
+    ball.draw(win)
+
+    pygame.display.update()
+
+def handle_collision(ball: Ball, left_paddle: Paddle, right_paddle: Paddle):
+    if ball.y + ball.radius >= HEIGHT or ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    # going to the left
+    if ball.x_vel < 0:
+        # check if the ball is in the range of the paddle's height
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            # check that the ball's edge hits the paddle's edge
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+
+    # going to the right
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+
+
+
 def handle_paddle_movement(keys, left_paddle: Paddle, right_paddle: Paddle):
     if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
         left_paddle.move(up=True)
@@ -33,7 +99,33 @@ def handle_paddle_movement(keys, left_paddle: Paddle, right_paddle: Paddle):
     if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.height + right_paddle.VEL <= HEIGHT:
         right_paddle.move(up=False)
 
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH , HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    ball = Ball(WIDTH//2, HEIGHT//2, BALL_RADIUS)
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle], ball)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
+
+        ball.move()
+
+        handle_collision(ball, left_paddle, right_paddle)
+
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
