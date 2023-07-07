38f1ccc188c0ed29a83a292bc4ccb58e304eaf33
Tomás Mónaco <monacotomas99@gmail.com>
2023-07-07 14:16:34 +0200

    Created paddles

--- solution.py
+++ solution.py
@@ -3,3 +3,37 @@ import pygame
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('PONG')
+
+class Paddle:
+    COLOR = WHITE
+    VEL = 4
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win): 
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height)) 
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+def handle_paddle_movement(keys, left_paddle: Paddle, right_paddle: Paddle):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.height + left_paddle.VEL <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.height + right_paddle.VEL <= HEIGHT:
+        right_paddle.move(up=False)
+
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH , HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
