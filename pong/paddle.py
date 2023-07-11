78f297662d3200bc4c227fd69a20efd92bb667a8
Tomás Mónaco <monacotomas99@gmail.com>
2023-07-11 12:05:57 +0200

    Added paddle class

---
+++ pong/paddle.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+class Paddle:
+    VEL = 4
+    WIDTH = 20
+    HEIGHT = 100
+    COLOR = (255, 255, 255)
+
+    def __init__(self, x, y) -> None:
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.COLOR, (self.x, self.y, self.WIDTH, self.HEIGHT))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
+    def reset(self):
+        self.x = self.original_y
+        self.y = self.original_y
